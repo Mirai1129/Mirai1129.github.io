00214bd09bb1d339c58ef0be9a93102248b8fca1
Mirai1129 <79797958+Mirai1129@users.noreply.github.com>
2022-05-17 14:40:43 +0800

    order

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ themeToggler.addEventListener('click', () => {
 
     themeToggler.querySelector('span').classList.toggle('active');
 })
+
+// fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.prductName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order/shipping}</td>
+                        <td class="primary">Details</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,43 @@
+const Orders = [{
+        prductName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        prductName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        prductName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        prductName: 'FDrone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        prductName: 'GPS 4k Drone',
+        productNumber: '22831',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        prductName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        prductName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
